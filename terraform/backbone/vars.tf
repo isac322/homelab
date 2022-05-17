de62b51b449ff7dcc1d828dfccc0d233e4784ef6
Byeonghoon Yoo <bhyoo@bhyoo.com>
2022-05-18 01:15:11 +0900

    docs(terraform): document terraform variables

--- terraform/backbone/vars.tf
+++ terraform/backbone/vars.tf
@@ -1,26 +1,34 @@
 variable "oracle_tenancy_ocid" {
-  type = string
+  type        = string
+  description = "OCID that can find at https://cloud.oracle.com/tenancy"
 }
 variable "oracle_user_ocid" {
-  type = string
+  type        = string
+  description = "OCID that can find at https://cloud.oracle.com/identity/users"
 }
 variable "oracle_fingerprint" {
-  type = string
+  type        = string
+  description = "Fingerprint of Signing key for OCI API key. Follow https://docs.oracle.com/en-us/iaas/Content/API/Concepts/apisigningkey.htm"
 }
 variable "oracle_private_key_b64" {
-  type = string
+  type        = string
+  description = "Signing key for OCI API key that encoded with base64. Follow https://docs.oracle.com/en-us/iaas/Content/API/Concepts/apisigningkey.htm"
 }
 variable "oracle_region" {
-  type = string
+  type        = string
+  description = "region of OCI"
 }
 
 # Required permissions for this token: "Zone Read", "API Tokens Write"
 variable "cloudflare_api_token" {
-  type = string
+  type        = string
+  description = "API token of Cloudflare. This token must have permission `API Tokens Write` and `Zone Read` at least. Follow https://developers.cloudflare.com/api/tokens/create/"
 }
 variable "cloudflare_host" {
-  type = string
+  type        = string
+  description = "Cloudflare Zone (host)"
 }
 variable "cloudflare_ca_api_key" {
-  type = string
+  type        = string
+  description = "Can get on `Origin CA Key` of https://dash.cloudflare.com/profile/api-tokens"
 }
