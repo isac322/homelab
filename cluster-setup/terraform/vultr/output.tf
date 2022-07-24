dc7f738c8d784d681d35a4a7c7084e20b6fdf577
Byeonghoon Yoo <bhyoo@bhyoo.com>
2022-07-25 01:30:39 +0900

    refactor: replace terraform with ansible

--- cluster-setup/terraform/vultr/output.tf
+++
@@ -1,4 +0,0 @@
-output "ip_backbone_master" {
-  value     = vultr_instance.backbone_master.main_ip
-  sensitive = true
-}
--- cluster-setup/terraform/wireguard/main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    wireguard = {
-      source = "OJFord/wireguard"
-      version = ">= 0.2.1, < 1.0.0"
-    }
-  }
-  required_version = ">= 1.1.8, < 2.0.0"
-}
