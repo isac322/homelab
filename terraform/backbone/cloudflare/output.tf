d02597279b0f999789218fde525937db48963b78
Byeonghoon Yoo <bhyoo@bhyoo.com>
2022-07-25 23:57:58 +0900

    feat(terraform): register cf api token for cert-manager

--- terraform/backbone/cloudflare/output.tf
+++ terraform/backbone/cloudflare/output.tf
@@ -2,3 +2,7 @@ output "external_dns_api_token" {
   value     = cloudflare_api_token.external_dns_api_token.value
   sensitive = true
 }
+output "cert_manager_api_token" {
+  value     = cloudflare_api_token.cert_manager_api_token.value
+  sensitive = true
+}
