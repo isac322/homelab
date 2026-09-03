0b5e6541302692af8365fbbed64343d742c6ea81
Byeonghoon Yoo <bhyoo@bhyoo.com>
2026-09-03 14:53:14 +0900

    chore(terraform): import GitHub App SSM parameters for ARC
    Adopt the manually created SSM parameters and TFC workspace variables
    into Terraform state so the next apply does not recreate them.

---
+++ 0-configure-saas/imports.tf
@@ -0,0 +1,14 @@
+import {
+  to = module.saas.tfe_variable.github_app_id_arc_cc_lb
+  id = "bhyoo/homelab-backbone/github_app_id_arc_cc_lb/terraform"
+}
+
+import {
+  to = module.saas.tfe_variable.github_app_installation_id_arc_cc_lb
+  id = "bhyoo/homelab-backbone/github_app_installation_id_arc_cc_lb/terraform"
+}
+
+import {
+  to = module.saas.tfe_variable.github_app_private_key_arc_cc_lb
+  id = "bhyoo/homelab-backbone/github_app_private_key_arc_cc_lb/terraform"
+}
--- 1-provision/env/backbone/imports.tf
+++ 1-provision/env/backbone/imports.tf
@@ -12,3 +12,18 @@ import {
   to = module.dns_secrets.aws_ssm_parameter.hermes_isacmes_jay_telegram_token[0]
   id = "/homelab/cluster/backbone/token/telegram/isacmes-jay"
 }
+
+import {
+  to = module.dns_secrets.aws_ssm_parameter.github_app_id_arc_cc_lb[0]
+  id = "/homelab/cluster/backbone/github-app/arc-cc-lb/id"
+}
+
+import {
+  to = module.dns_secrets.aws_ssm_parameter.github_app_installation_id_arc_cc_lb[0]
+  id = "/homelab/cluster/backbone/github-app/arc-cc-lb/installation-id"
+}
+
+import {
+  to = module.dns_secrets.aws_ssm_parameter.github_app_private_key_arc_cc_lb[0]
+  id = "/homelab/cluster/backbone/github-app/arc-cc-lb/private-key"
+}
